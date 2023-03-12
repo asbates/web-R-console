694950f1d0261944207ca39e74410df26df1f175
Andrew Bates <andrewbates73@gmail.com>
2023-03-12 12:08:41 -0700

    preview config

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [react(), crossOriginIsolation()],
+  plugins: [react(), crossOriginIsolation(), topLevelAwait()],
+  preview: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
 });
